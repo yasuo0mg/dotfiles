4f9c854ff47bcd4a0496b12e3730acbe201fc03a
omoriyasuo <omoriyasuo.dev@gmail.com>
2021-03-19 00:41:07 +0900

    fix prompt

--- zsh.d/prompt.zsh
+++ zsh.d/prompt.zsh
@@ -7,7 +7,7 @@ precmd () {
   LANG=en_US.UTF-8 vcs_info
   [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
 }
-PROMPT="%{${fg[yellow]}%}٩(｡•́‿•̀｡)۶[%~%1(v|%F{green}%1v%f|)%{${fg[yellow]}%}]%{${reset_color}%}%b "
+PROMPT="%{${fg[yellow]}%}[%~%1(v|%F{green}%1v%f|)%{${fg[yellow]}%}]%{${reset_color}%}%b "
 
 # 自動補完
 autoload -U compinit
