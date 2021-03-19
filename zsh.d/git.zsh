61449225b46b36b17f8acfa026892891c59face8
omoriyasuo <omoriyasuo.dev@gmail.com>
2021-03-19 15:20:22 +0900

    fix

--- zsh.d/git.zsh
+++ zsh.d/git.zsh
@@ -3,7 +3,7 @@ alias g='git'
 alias gcm='git commit -a -m'
 alias delete-merged-branches="git branch --merged | grep -v '*' | xargs -I % git branch -d %"
 alias gadd='git add .'
-alias gip='git push origin `git rev-parse --abbrev-ref HEAD`'
+alias gp='git push origin `git rev-parse --abbrev-ref HEAD`'
 alias gst='git status'
 alias gdiff='git diff'
 
