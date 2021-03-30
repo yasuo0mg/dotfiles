7f097765a37f943371db9a327c921125f5bda7e3
omoriyasuo <omoriyasuo.dev@gmail.com>
2021-03-30 19:50:09 +0900

    update karabiner setting

--- link.sh
+++ link.sh
@@ -9,3 +9,5 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -sf ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+ln -sf ~/dotfiles/karabiner/assets/ ~/.config/karabiner/assets
+ln -sf ~/dotfiles/karabiner/automatic_backups ~/.config/karabiner/automatic_backups
