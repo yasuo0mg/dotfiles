3351a0791d5adab6189a868a5a941e25971c4e5e
omoriyasuo <omoriyasuo.dev@gmail.com>
2021-06-15 22:54:55 +0900

    set asdf path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -12,3 +12,8 @@ export PATH=$PATH:$HOME/flutter/bin
 export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+fpath=(
+  $(brew --prefix asdf)/etc/bash_completion.d
+  $fpath
+)
+$(brew --prefix asdf)/asdf.sh
