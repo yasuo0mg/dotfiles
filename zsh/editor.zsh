57e7d05ee1a1f0e0ef8cdd2ba1f7ef0f00be0b74
omoriyasuo <omoriyasuo.dev@gmail.com>
2021-09-13 19:17:52 +0900

    set asdf path

--- zsh/editor.zsh
+++ zsh/editor.zsh
@@ -1,1 +1,2 @@
 export EDITOR=vim
+eval "$(direnv hook zsh)"
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,6 +1,6 @@
 export MANPATH=/opt/local/man:$MANPATH
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+# export PATH="$HOME/.rbenv/bin:$PATH"jj
+# eval "$(rbenv init -)"
 export PATH=$PATH:/usr/local/bin/mysql
 export PATH=$PATH:/opt/homebrew/bin
 export PATH=/opt/local/bin:/opt/local/sbin/:$PATH
@@ -9,19 +9,16 @@ export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:/bin:/usr/bin:/usr/local/bin
 export PATH=$PATH:$HOME/flutter/bin
-export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-fpath=(
-  $(brew --prefix asdf)/etc/bash_completion.d
-  $fpath
-)
-$(brew --prefix asdf)/asdf.sh
+# export NVM_DIR="$HOME/.nvm"
+  # [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  # [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-# asdf
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
+############
+### asdf ###
+############
+# https://asdf-vm.com/guide/getting-started.html#_3-install-asdf
 
-export ASDF_DATA_DIR=/opt/asdf-data
-. /opt/asdf/asdf.sh
-. /opt/asdf/completions/asdf.bash
+# zsh (not using oh-my-zsh) & brew
+# . $HOME/.asdf/asdf.sh
+# $(brew --prefix asdf)/asdf.sh
+. /usr/local/opt/asdf/asdf.sh
