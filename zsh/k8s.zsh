723380bf854ef47f138ec310d9d58a7296371b33
omoriyasuo <omoriyasuo.dev@gmail.com>
2021-10-18 11:09:00 +0900

    Set kube-ps1 config

--- zsh/k8s.zsh
+++ zsh/k8s.zsh
@@ -1,3 +1,11 @@
 alias k='kubectl'
 
 complete -F __start_kubectl k
+
+# PROMPTにcontext/namespaceを表示するための設定
+if [[ -s "/usr/local/opt/kube-ps1/share/kube-ps1.sh" ]]; then
+  export KUBE_PS1_CTX_COLOR='13'
+  export KUBE_PS1_SYMBOL_COLOR='cyan'
+  source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
+fi
+export PROMPT="$(kube_ps1) $PROMPT"
