8ec9619cd4f70f2daec94f481e52caa273afc220
omoriyasuo <omoriyasuo.dev@gmail.com>
2021-10-31 23:50:30 +0900

    Add android studio path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,6 +1,4 @@
 export MANPATH=/opt/local/man:$MANPATH
-# export PATH="$HOME/.rbenv/bin:$PATH"jj
-# eval "$(rbenv init -)"
 export PATH=$PATH:/usr/local/bin/mysql
 export PATH=$PATH:/opt/homebrew/bin
 export PATH=/opt/local/bin:/opt/local/sbin/:$PATH
@@ -9,9 +7,7 @@ export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:/bin:/usr/bin:/usr/local/bin
 export PATH=$PATH:$HOME/flutter/bin
-# export NVM_DIR="$HOME/.nvm"
-  # [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  # [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+export PATH=$PATH:/Users/yasuo/Library/Android/sdk/platform-tools
 
 ############
 ### asdf ###
