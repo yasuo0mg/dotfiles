1efc5bbddb953729b7483de21246ed5a497cea51
omoriyasuo <omoriyasuo.dev@gmail.com>
2022-09-08 22:55:13 +0900

    add comment

--- link.sh
+++ link.sh
@@ -9,6 +9,7 @@ ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
 ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -sf ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -sf ~/dotfiles/karabiner/assets/complex_modifications/1572145077.json ~/.config/karabiner/assets/complex_modifications
+# https://qiita.com/wawawa/items/50c2c612b0937f28d92b
 ln -sf /usr/local/bin/gcc-11 /usr/local/bin/gcc
 ln -sf /usr/local/bin/g++-11 /usr/local/bin/g++
 # ln -sf $(ls -d /usr/local/bin/* | grep "/g++-" | sort -r | head -n1) /usr/local/bin/g++
